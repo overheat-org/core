08902915b230be5e56191862198d2f6346301646
/Nexus_Prime <nicolas.estevao.lima1004@hotmail.com>
2023-09-15 21:45:05 -0300

    Remake loader

--- src/lib/loader.ts
+++ src/lib/loader.ts
@@ -1,63 +1,101 @@
-import { readdirSync } from "fs";
-import Bot from "./client";
-import path from "path";
+import fs from "fs";
+import DJS from 'discord.js';
+import ProductionClient from "./client/production";
+import { join as j, basename } from "path";
 import { Command, Event } from "./structures";
+import { readdir } from "fs/promises";
+
+type File = { path: string, parent: string };
 
 class Loader {
-	constructor(
-		private client: Bot
-	) { }
+	constructor(private client: ProductionClient, cached = false) {}
 
-	loadCommands(dirpath: string) {
-		const allFileNames = readdirSync(dirpath)
+	async readDir(path: string, options = { forFile: false }) {
+		const joinOnDir = async (path: string, parent: string) => {
+			const files = new Array<File>();
+			const readDir = await readdir(path, { withFileTypes: true });
+			
+			for(const dirent of readDir) {
+				if(dirent.isFile()) {
+					files.push({
+						path: j(path, dirent.name),
+						parent
+					})
+				}
+				else if(dirent.isDirectory()) {
+					if(dirent.name.startsWith('#')) return;
 
-		for (let i = 0; i < allFileNames.length; i++) {
-			const filePath = path.join(dirpath, allFileNames[i]);
-
-			const guildTestId = process.env.GUILD_TEST;
-			if (!guildTestId) {
-				console.log('warn: enviroment variable "GUILD_TEST" not specified')
+					await joinOnDir(path, parent);
+				}
 			}
 
-			import(filePath).then(f => {
-				if (!f.default) {
-					throw new Error(`File "${filePath}" hasn't default export`);
-				}
+			return files;
+		}
 
-				const command: Command = f.default;
-				Command.cache.set(command.data.name, command);
+		const dir = await joinOnDir(path, basename(path));
+		
+		switch (basename(path)) {
+			case 'commands':
+				dir?.forEach(this.loadCommand)
+				return;
+				
+			case 'events':
+				dir?.forEach(this.loadEvent)
+				return;
+		}
 
-				if (!guildTestId) return;
-
-				(this.client.guilds.fetch(process.env.GUILD_TEST as string)).then(g => {
-					const fetchedCommand = g.commands.cache.find(c => c.name == g.name);
-
-
-					if (fetchedCommand) {
-						g.commands.edit(fetchedCommand.id, command.data)
-					} else {
-						g.commands.create(command.data);
-					}
-				})
-			})
+		if(options.forFile) {
+			dir?.filter(c => /\.evt\.(js|ts)$/.test(c.path)).forEach(this.loadEvent)
+			dir?.filter(c => /\.cmd\.(js|ts)$/.test(c.path)).forEach(this.loadCommand)
 		}
 	}
+	
+	loadCommand(file: File) {
+		import(file.path).then(async (f) => {
+			if (!f.default) {
+				throw new Error(`File "${file.path}" hasn't default export`);
+			}
 
-	loadEvents(dirpath: string) {
-		const allFileNames = readdirSync(dirpath)
+			const command: Command = f.default;
+			Command.cache.set(command.data.name, command);
 
-		for (let i = 0; i < allFileNames.length; i++) {
-			const filePath = path.join(dirpath, allFileNames[i]);
+			let targetRegister: DJS.GuildApplicationCommandManager | DJS.ApplicationCommandManager<DJS.ApplicationCommand<{ guild: DJS.GuildResolvable; }>, { guild: DJS.GuildResolvable; }, null>;
+			if (process.env.NODE_ENV == 'development') {
+				const guildTestId = process.env.GUILD_TEST;
 
-			import(filePath).then(f => {
-				if (!f.default) {
-					throw new Error(`File "${filePath}" hasn't default export`);
+				if (!guildTestId) {
+					throw new Error('Enviroment variable "GUILD_TEST" not specified');
 				}
 
-				const event: Event<any> = f.default;
-				this.client[event.data.once ? "once" : "on"](event.data.type, (...args: any) => event.run(...args));
-			})
-		}
+				targetRegister = (await this.client.guilds.fetch(guildTestId)).commands;
+			} else {
+				targetRegister = this.client.application!.commands;
+			}
+
+			const fetchedCommand = targetRegister.cache.find(
+				(c) => c.name == command.data.name
+			);
+
+			if (fetchedCommand) {
+				targetRegister.edit(fetchedCommand.id, command.data);
+			} else {
+				targetRegister.create(command.data);
+			}
+		});
+	}
+
+	loadEvent(file: File) {
+		import(file.path).then((f) => {
+			if (!f.default) {
+				throw new Error(`File "${file.path}" hasn't default export`);
+			}
+
+			const event: Event<any> = f.default;
+			this.client[event.data.once ? "once" : "on"](
+				event.data.type,
+				(...args: any) => event.run(...args)
+			);
+		});
 	}
 }
 
