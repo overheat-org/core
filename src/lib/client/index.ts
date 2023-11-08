76b48c082b779ab6c83bdac38527b1f920cb5ea8
/Nexus_Prime <nicolas.estevao.lima1004@hotmail.com>
2023-11-08 15:22:19 -0300

    Downgraded Client

--- src/lib/client/index.ts
+++
@@ -1,8 +0,0 @@
-import DevelopmentClient from "./development";
-import ProductionClient from "./production";
-
-const Client = process.env.NODE_ENV == 'development'
-	? DevelopmentClient
-	: ProductionClient;
-
-export default Client;
