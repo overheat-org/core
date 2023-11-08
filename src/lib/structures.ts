e0ef6306e09e72c889dde535a64fae3a241e25d8
/Nexus_Prime <nicolas.estevao.lima1004@hotmail.com>
2023-11-08 15:21:56 -0300

    Removed unused structure

--- src/lib/structures.ts
+++ src/lib/structures.ts
@@ -1,71 +1,5 @@
 import DJS from "discord.js";
-import _client from "./client/production";
-
-export class ResponseManager {
-	interaction: DJS.CommandInteraction;
-	private ephemeral: boolean = false;
-
-	setEphemeral(boolean: boolean) {
-		this.ephemeral = boolean
-	}
-
-	constructor(interaction: DJS.CommandInteraction) {
-		this.interaction = interaction;
-	}
-
-	success(data: string | DJS.EmbedData, linkButton?: { url: string, label: string }) {
-		this.interaction.reply({
-			embeds: [this.generateEmbed({
-				data,
-				defaultTitle: 'Sucesso',
-				color: '#1cff95'
-			})],
-			components: linkButton ? [
-				new DJS.ActionRowBuilder<DJS.ButtonBuilder>().addComponents(
-					new DJS.ButtonBuilder()
-						.setStyle(DJS.ButtonStyle.Link)
-						.setLabel(linkButton.label)
-						.setURL(linkButton.url)
-				)
-			] : undefined,
-			ephemeral: this.ephemeral
-		})
-	}
-
-	error(data: string | DJS.EmbedData) {
-		this.interaction.reply({
-			embeds: [this.generateEmbed({
-				data,
-				defaultTitle: 'Erro',
-				color: '#ff1c37'
-			})],
-			ephemeral: this.ephemeral
-		})
-	}
-
-	generateEmbed(obj: { data: string | DJS.EmbedData, color: DJS.ColorResolvable, defaultTitle: string }) {
-		const embed = typeof obj.data == 'string'
-			? obj.data.length > 20
-				? new DJS.EmbedBuilder()
-					.setTitle(obj.defaultTitle)
-					.setDescription(obj.data)
-					.setColor(obj.color)
-					.setFooter({
-						text: this.interaction.user.username,
-						iconURL: this.interaction.user.avatarURL() ?? this.interaction.user.defaultAvatarURL
-					})
-				: new DJS.EmbedBuilder()
-					.setAuthor({
-						name: obj.data,
-						iconURL: this.interaction.user.avatarURL() ?? this.interaction.user.defaultAvatarURL
-					})
-					.setColor(obj.color)
-			: new DJS.EmbedBuilder(obj.data)
-				.setColor(obj.data.color ? obj.data.color : obj.color)
-
-		return embed;
-	}
-}
+import _client from "./client";
 
 export declare class CommandData {
 	name: string;
@@ -84,7 +18,6 @@ export class Command {
 	data: CommandData;
 	run: (obj: {
 		interaction: DJS.ChatInputCommandInteraction<'cached'>,
-		response: ResponseManager
 	}) => any;
 	autocomplete?: (interaction: DJS.AutocompleteInteraction<'cached'>) => any;
 
